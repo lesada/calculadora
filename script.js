9852321f6fe66aa8ee54c76fba2b4bffe2281468
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-11-07 01:41:15 -0300

    Melhorando funções

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function onOff() {
     var button = document.getElementById("onOff");
-    
-    button.innerText == "On" ? button.innerText = "Off" : button.innerText = "On";   
+    var display = document.getElementById("display");
+
+    button.innerText == "On" ? button.innerText = "Off" : button.innerText = "On";
+
+    if (button.innerText == "On") {
+        display.style.backgroundColor = "#313131";
+        clearNumeros();
+    }
+    else {
+        display.style.backgroundColor = "#798369";
+        clearNumeros();
+
+    }
 }
 
-function displayNumeros(num) {
+function displayButtons(button) {
     var display = document.getElementById("display");
-    display.value += num;
+    display.value += button;
 }
 
 function clearNumeros() {
