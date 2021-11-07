7e3108bbc83e6fa099fb6ca23e21a7f8b6641010
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-11-07 01:19:10 -0300

    Botão de limpar

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function onOff() {
     
     button.innerText == "On" ? button.innerText = "Off" : button.innerText = "On";   
 }
+
+function displayNumeros(num) {
+    var display = document.getElementById("display");
+    display.value += num;
+}
+
+function clearNumeros() {
+    var display = document.getElementById("display");
+    display.value = "";
+}
