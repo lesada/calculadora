a3e2b77c43995b907028dad461ed5582c446f05e
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-11-15 11:13:32 -0300

    Alterando aspas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function onOff() {
-    let isOnOff = document.getElementById("onOff");
-    let display = document.getElementById("display");
+let display = document.getElementById("display");
+let isOnOff = document.getElementById("onOff");
 
+function onOff() {
     isOnOff.innerText == "On" ? isOnOff.innerText = "Off" : isOnOff.innerText = "On";
 
     if (isOnOff.innerText == "On") {
@@ -16,13 +16,11 @@ function onOff() {
 }
 
 function displayButtons(button) {
-    let isOnOff = document.getElementById("onOff");
-    let display = document.getElementById("display");
     let pattern = new RegExp("[+-\/\*]");
 
-    if ((pattern.test(display.value)) && (button == "+" || button == "-" || button == "*" || button == "/")) {
-     calcular();
-    }
+    if ((pattern.test(display.value)) && (button == "+" || button == "-" || button == "*" || button == "/"))
+        calcular();
+
 
     if (isOnOff.innerText == "Off")
         display.value += button;
@@ -31,14 +29,11 @@ function displayButtons(button) {
 
 
 function clearNumeros() {
-    let display = document.getElementById("display");
     display.value = "";
 }
 
 function calcular() {
-    let display = document.getElementById("display");
     display.value = eval(display.value);
-
 
 }
 
@@ -46,80 +41,81 @@ document.addEventListener('keydown', function (event) {
     switch (event.keyCode) {
         case 48:
         case 96:
-            displayButtons("0");
+            displayButtons(0);
             break;
 
         case 49:
         case 97:
-            displayButtons("1");
+            displayButtons(1);
             break;
 
         case 50:
         case 98:
-            displayButtons("2");
+            displayButtons(2);
             break;
 
         case 51:
         case 99:
-            displayButtons("3");
+            displayButtons(3);
             break;
 
         case 52:
         case 100:
-            displayButtons("4");
+            displayButtons(4);
             break;
 
         case 53:
         case 101:
-            displayButtons("5");
+            displayButtons(5);
             break;
 
         case 54:
         case 102:
-            displayButtons("6");
+            displayButtons(6);
             break;
 
         case 55:
         case 103:
-            displayButtons("7");
+            displayButtons(7);
             break;
 
         case 56:
         case 104:
-            displayButtons("8");
+            displayButtons(8);
             break;
 
         case 57:
         case 105:
-            displayButtons("9");
+            displayButtons(9);
             break;
 
         case 106:
-            displayButtons("*");
+            displayButtons('*');
             break;
 
         case 107:
         case 187:
-            displayButtons("+");
+            displayButtons('+');
             break;
 
         case 109:
         case 189:
-            displayButtons("-");
+            displayButtons('-');
             break;
 
 
         case 111:
         case 193:
-            displayButtons("/");
+            displayButtons('/');
             break;
 
         case 190:
-            displayButtons(".");
+            displayButtons('.');
             break;
 
-
-
+        case 13:
+            calcular();
+            break; 
 
         default: return
 
